fb74f316428aa520f768e0d464d8a3b52819d706
aibekovislam <islamxdaamduu@gmail.com>
2024-05-11 15:15:38 +0600

    modified favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ReduxProvider } from "./redux/provider";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Алтын уя - пожертвование в Курбан",
@@ -24,15 +25,17 @@ function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <link rel='canonical' href='https://www.qurban.kg/' />
-      <meta name="description" content="Сделайте доброе дело и совершите пожертвование в курбан айт. Поддержите благотворительность прямо сейчас! Помогите нуждающимся и сделайте пожертвование в курбан айт. Ваш вклад в благотворительность важен! Поддержите благотворительный проект и совершите пожертвование в курбан айт. Вместе мы можем помочь тем, кто в беде." />
-      <meta name="keywords" content="Алтын уя, алтын уя, курбан, курбан алтын уя, курбан айт, altyn uya, qurban, kyrgyz charity, qurban altyn uya, charity, пожертвование, пожертвования, курман айт, курман, алтын уя курман, курман алтын уя, пожертвовать в благотворительность, благотворительность" />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-      />
-      <title>Алтын уя - сбор пожертвование в Курбан</title>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel='canonical' href='https://www.qurban.kg/' />
+        <meta name="description" content="Сделайте доброе дело и совершите пожертвование в курбан айт. Поддержите благотворительность прямо сейчас! Помогите нуждающимся и сделайте пожертвование в курбан айт. Ваш вклад в благотворительность важен! Поддержите благотворительный проект и совершите пожертвование в курбан айт. Вместе мы можем помочь тем, кто в беде." />
+        <meta name="keywords" content="Алтын уя, алтын уя, курбан, курбан алтын уя, курбан айт, altyn uya, qurban, kyrgyz charity, qurban altyn uya, charity, пожертвование, пожертвования, курман айт, курман, алтын уя курман, курман алтын уя, пожертвовать в благотворительность, благотворительность" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
+        <title>Алтын уя - сбор пожертвование в Курбан</title>
+      </Head>
       <body>
         <ReduxProvider>
           {children}
